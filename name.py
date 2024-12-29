305d1d65a8e9d876991260b44270fd99786c873b
tammy-245678 <abdullahbigman38@gmail.com>
2024-12-29 21:22:39 +0500

    commit

---
+++ name.py
@@ -0,0 +1,12 @@
+my_dict = {}
+my_dict = {1: 'apple', 2: 'ball'}
+my_dict = {
+'name': 'John',
+1: [2, 4, 3]
+}
+print(my_dict['name'])
+print(my_dict.get(1))
+my_dict['name'] = "Tanvir"
+print(my_dict)
+my_dict['address'] = 'Downtown'
+print(my_dict)
